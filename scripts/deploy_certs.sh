f7e3cf7784d6e66ce9e9f888852a5319b03a6728
pbsurf <>
2026-08-22 14:32:59 -0700

    more fixes

--- scripts/deploy_certs.sh
+++ scripts/deploy_certs.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+set -euo pipefail
 
 REMOTE_SERVERS=("tiles-b.styluslabs.com")
 CERT_PATH="/etc/nginx/ssl"
 
 echo "Installing certs locally..."
+sudo mkdir -p $CERT_PATH
 #sudo cp $HOME/.acme.sh/tiles.styluslabs.com_ecc/fullchain.cer $CERT_PATH/tiles.styluslabs.com.fullchain.pem
 #sudo cp $HOME/.acme.sh/tiles.styluslabs.com_ecc/tiles.styluslabs.com.key $CERT_PATH/tiles.styluslabs.com.key.pem
-sudo cp $HOME/maps/cert.pem $CERT_PATH/tiles.styluslabs.com.fullchain.pem
-sudo cp $HOME/maps/key.pem $CERT_PATH/tiles.styluslabs.com.key.pem
+sudo cp -f $HOME/maps/tiles.styluslabs.com.*.pem $CERT_PATH
 sudo systemctl reload nginx
 
 for server in "${REMOTE_SERVERS[@]}"; do
     echo "Deploying TLS certs to $server..."
-    (cd $CERT_PATH && sudo rsync --mkpath --rsync-path="sudo rsync" tiles.styluslabs.com.* $server:$CERT_PATH/)
+    rsync --mkpath --rsync-path="sudo rsync" $HOME/maps/tiles.styluslabs.com.*.pem $USER@$server:$CERT_PATH/
     # Reload remote Nginx (reload tells service to just reload its config instead of completely restarting, if supported)
     ssh $server "sudo systemctl reload nginx"
 done
